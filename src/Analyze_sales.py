d0419f30a5a062c19f5b39c64efb829bc5b796f7
PlayerModel-021 <geanraz0721@gmail.com>
2026-03-10 23:33:44 +0800

    Added the CLI interaction to modify the table but not final V1, oh and I also made the processes as functions

--- src/Analyze_sales.py
+++ src/Analyze_sales.py
@@ -11,24 +11,51 @@ datapath = BASE_DIR / "data" / "sales.csv"
 #we are gonna first load the dataset from /data
 df = pd.read_csv(datapath)
 
-#first five rows of the dataset
-print("Dataset Head Preview:" )
-print(df.head())
-
 #added a revenue column
 df["revenue"] = df["price"] * df["quantity"]
-print("\nnow with revenue:")
-print(df)
 
-#Total revenue
-totalrev = df["revenue"].sum()
-print(f"\nTotal Revenue would be: {totalrev}")
+def datesum():
+    dates = df["date"].sum()
+    return dates
 
-#final initialization
-initial = df
 
-outputdir = BASE_DIR / "output"
-outputdir.mkdir(exist_ok=True)
-pathtoreport = outputdir / "Product-with-revenues.csv"
-initial.to_csv(pathtoreport)
-print(f"\nreport exported to {pathtoreport}")
+def datahead():
+    print("Dataset Head Preview:" )
+    print(df.head())
+
+def revenue():
+
+    #Total revenue
+    totalrev = df["revenue"].sum()
+    return totalrev
+
+def columnsum(col):
+    print(df[col].sum())
+
+
+while True:
+    print(f"1. Get Revenues | 2. View Dataset | 3. Structure the table | 4. Get sum of each column")
+    answer = int(input(f"Choose an Option to do with the file (1,2,3,4): "))
+
+    if answer == 1:
+        print(revenue())
+    elif answer == 2:
+        print(df)
+    elif answer == 3:
+        break
+    elif answer == 4:
+        print(f"1. product | 2. price | 3. quantity")
+        answercolumn = int(input(f"Which column to sum?: (1,2,3): "))
+
+        match answercolumn:
+            case 1:
+                print(f"\nThis is the total count of all products")
+                columnsum("product")
+            case 2:
+                print(f"\nThis is the total sum of all prices")
+                columnsum("price")
+            case 3: 
+                print(f"\nTotal Quantity of all products")
+                columnsum("quantity")
+    else:
+        print("invalid option please choose between 1 - 4")
