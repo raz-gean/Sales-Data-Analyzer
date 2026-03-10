e490e2e1755982cfff69e431306d460705cde509
PlayerModel-021 <geanraz0721@gmail.com>
2026-03-11 02:25:09 +0800

    added TotalSalesPerProduct group, also error handling on the options

--- src/Analyze_sales.py
+++ src/Analyze_sales.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
-#Root Base
+#Root Base of the file dont change the damn parent wtf
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 #change this to your path to wherever your csv is located
@@ -12,38 +12,42 @@ datapath = BASE_DIR / "data" / "sales.csv"
 df = pd.read_csv(datapath)
 
 #added a revenue column
-df["revenue"] = df["price"] * df["quantity"]
+df["TotalPricePerProduct"] = df["price"] * df["quantity"]
 
-def datesum():
-    dates = df["date"].sum()
-    return dates
+def Total_sale_prices_perday():
+    totalperday = df.groupby(["date", "product"])["TotalPricePerProduct"].sum()
+    print(totalperday)
 
-
+#Didnt actually use this as a feature yet but we can add this much laterr hmm
 def datahead():
     print("Dataset Head Preview:" )
     print(df.head())
 
 def revenue():
-
     #Total revenue
-    totalrev = df["revenue"].sum()
+    totalrev = df["TotalPricePerProduct"].sum()
     return totalrev
 
+#This is the dynamic column to like sum the prices and stuff dafuq, dont change this if you you just gonna make it static
 def columnsum(col):
     print(df[col].sum())
 
 
 while True:
     print(f"1. Get Revenues | 2. View Dataset | 3. Structure the table | 4. Get sum of each column")
-    answer = int(input(f"Choose an Option to do with the file (1,2,3,4): "))
+    try:
+        answer = int(input(f"Choose an Option to do with the file (1,2,3,4): "))
+    except ValueError:
+        print("Invalid answer")
+        continue
 
     if answer == 1:
         print(revenue())
     elif answer == 2:
         print(df)
-    elif answer == 3:
-        break
-    elif answer == 4:
+    elif answer == 3:  #This part of it is experimental for now since it so so broad to like structure much tables if the csv or dataset has many columns with relations
+        Total_sale_prices_perday()
+    elif answer == 4: #Get sum of the computing columns but not every column tho so watch out
         print(f"1. product | 2. price | 3. quantity")
         answercolumn = int(input(f"Which column to sum?: (1,2,3): "))
 
